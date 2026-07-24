64e0163feb21d3e2e699696824b7745c8caecd97
AbidShaik09 <hussainabidshaik@gmail.com>
2026-07-24 23:14:50 +0530

    Added More Pages & guestr Guard for better User Experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,40 @@
 import { Routes } from '@angular/router';
-import { Login }  from './pages/login/login'
-import { Register }  from './pages/register/register'
-import { Dashboard }  from './pages/dashboard/dashboard'
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
+import { Profile } from './pages/profile/profile';
+import { Settings } from './pages/settings/settings';
+import { guestGuard } from './guards/guest-guard';
 
 export const routes: Routes = [
-    {
-        path:'login',
-        component:Login,
-    },
-    {
-        path:'register',
-        component:Register,
-    },
-    {
-        path:'dashboard',
-        component:Dashboard,
-    },
-    {
-        path:'',
-        redirectTo:'login',
-        pathMatch: 'full'
-    },
-    {
-        path:"**",
-        redirectTo: 'login',
-    }
+  {
+    path: 'login',
+    component: Login,
+    canActivate: [guestGuard], // Ensure you have a guest guard to protect this route
+  },
+  {
+    path: 'register',
+    component: Register,
+    canActivate: [guestGuard],
+  },
+  {
+    path: '',
+    component: Dashboard,
+    canActivate: [authGuard], // Ensure you have an auth guard to protect this route
+  },
+  {
+    path: 'profile',
+    component: Profile,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'settings',
+    component: Settings,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
